9d3f314050488a474758e09f30c0cc7680494618
Hemadri <starksense1325@gmail.com>
2026-06-07 17:56:12 +0530

    update the URL

--- RIC_Project/Frontend/src/services/api.js
+++ RIC_Project/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "http://ric.appvirtualex.com"
 });
 
 api.interceptors.request.use(config => {
